c3b069bac473d488cdc82baf958bf857dc5a21fc
Jerremyaponte <Jerremy.Aponte@upr.edu>
2026-03-12 22:52:18 -0400

    Phase 1 - Complete
    Made sure to reset the enemies once the game is restarted, as well implemented that the enemies hitbox wouldnt collide with the player.

--- src/Program.cpp
+++ src/Program.cpp
@@ -17,9 +17,8 @@ Program::Program() {
         });
 
     for (int i = 0; i < 30; i++) {
-        float x = 250 + 50 * i;
-        float y = 200 + 50 * i;
-
+        float x = 250 + 50 * (i % 10); // resets x every 10 enemies
+        float y = 200 + 50 * (i / 10); // resets y after every 10 enemy killed
         Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
             std::pair<float, float>{x, y}, 
             new StdEnemy(x, y)
@@ -56,9 +55,12 @@ void Program::Update() {
         }
 
         for (Projectile& p : Projectile::projectiles) { 
-            p.update(); 
+            p.update();
 
+        if (p.ID != 0 && HitBox::Collision(player->hitBox, p.getHitBox())) {
+            PlayerReset();
         }
+}
 
         if (lives <= 0 && pauseFrames <= 0) gameOver = true;
         Projectile::CleanProjectiles();
@@ -181,7 +183,29 @@ void Program::PlayerReset() {
 void Program::Reset() {
     Enemy::enemies.clear();
     StdEnemy::attackInProgress = false;
+
     player = new Player((GetScreenWidth() / 2) - 15, GetScreenHeight() * 0.75f);
+
+    Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+        std::pair<float, float>{350, 150}, 
+        new SpEnemy(350, 150)
+    });
+
+    Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+        std::pair<float, float>{600, 150}, 
+        new SpEnemy(600, 150)
+    });
+
+    for (int i = 0; i < 30; i++) {
+        float x = 250 + 50 * (i % 10);
+        float y = 200 + 50 * (i / 10);
+
+        Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+            std::pair<float, float>{x, y}, 
+            new StdEnemy(x, y)
+        });
+    }
+
     respawnCooldown = 1080;
     respawns = 0;
     count = 0;
